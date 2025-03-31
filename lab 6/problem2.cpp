c594f00d83fddca4d1049d600a5de73c882014a8
Sergiu Tuduce <sergiutuduce21@gmail.com>
2025-03-31 19:47:50 +0300

    last commit of lab 6

--- lab 6/problem2.cpp
+++ lab 6/problem2.cpp
@@ -3,16 +3,109 @@
 
 using namespace std;
 
+unsigned int modullus(int x) {
+    return (x>0) ? x : -1*x;
+}
+
 class ChessPiece {
+    string VALID_PIECES[6] = {"Pawn", "Bishop", "Knight", "Rook", "King", "Queen"};
     protected:
-        int m_positionX;
-        int m_positionY;
+        unsigned int m_positionX;
+        unsigned int m_positionY;
         string m_name;
     private:
         char m_color;
     public:
-        ChessPiece(int positionX, int positionY, string name, char color):
+        ChessPiece(unsigned int positionX, unsigned int positionY, string name, char color):
         m_positionX{positionX}, m_positionY{positionY}, m_name{name}, m_color{color}{}
 
-        
+        bool movePiece(unsigned int newX, unsigned int newY) {
+            return false;
+        }
+
+        unsigned int getPositionX() const {
+            return this->m_positionX;
+        }
+
+        unsigned int getPositionY() const {
+            return this->m_positionY;
+        }
+
+        string getName() const {
+            return this->m_name;
+        }
+
+        char getColor() const {
+            return this->m_color;
+        }
+
+        bool setX(unsigned int newX) {
+            if (1 <= newX and newX <= 8) {
+                this->m_positionX = newX;
+                return true;
+            }
+            return false;
+        }
+
+        bool setY(unsigned int newY) {
+            if (1 <= newY and newY <= 8) {
+                this->m_positionY = newY;
+                return true;
+            }
+            return false;
+        }
+
+        bool setName(string newName) {
+            for (int i = 0; i < 6; i ++) {
+                if (newName == VALID_PIECES[i]) {
+                    this->m_name = newName;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        bool setColor(char newColor) {
+            if (newColor != 'b' and newColor != 'w') {
+                return false;
+            }
+            this->m_color = newColor;
+            return true;
+        }
+
+
 };
+
+class Bishop : public ChessPiece {
+    public:
+        Bishop(unsigned int positionX, unsigned int positionY, char color) :
+        ChessPiece{positionX, positionY, "Bishop", color}{}
+        bool movePiece(unsigned int newX, unsigned int newY) {
+            if (newX < 1 or newX > 8 or newY < 1 or newY > 8)
+                return false;
+            return (modullus(newX - this->m_positionX) == modullus(newY - this->m_positionY));
+        }
+};
+
+class Rook : public ChessPiece {
+    public:
+    Rook(unsigned int positionX, unsigned int positionY, char color) :
+    ChessPiece{positionX, positionY, "Rook", color}{}
+    bool movePiece(unsigned int newX, unsigned int newY) {
+        if (newX < 1 or newX > 8 or newY < 1 or newY > 8)
+            return false;
+        return (newX - this->m_positionX) * (newY - this->m_positionY) == 0;
+    }
+};
+
+class Pawn : public ChessPiece {
+    public:
+    Pawn(unsigned int positionX, unsigned int positionY, char color) :
+    ChessPiece{positionX, positionY, "Pawn", color}{}
+    bool movePiece(unsigned int newX, unsigned int newY) {
+        if (newX < 1 or newX > 8 or newY < 1 or newY > 8)
+            return false;
+        return (this->getColor() == 'w' and newX == this->m_positionX - 1) or (this->getColor() == 'b' and newX == this->m_positionX + 1);
+    }
+};
+
